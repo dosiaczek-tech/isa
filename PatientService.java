24c6ed08c153455577520b6a57ca00a17a1d550b
Dominika <burakiewicz.dominika@gmail.com>
2020-01-19 12:37:24 +0100

    Update doctors and patients

--- PatientService.java
+++ PatientService.java
@@ -5,10 +5,12 @@ class PatientService {
     private final List<Patient> patients = new ArrayList<>();
 
     PatientService() {
-        this.patients.add(new Patient("Jan", "Kowalski", 11));
-        this.patients.add(new Patient("Adam", "Nowak", 12));
+        this.patients.add(new Patient("Jan", "Kowalski", 94));
         this.patients.add(new Patient("Maria", "Nowakowska", 13));
         this.patients.add(new Patient("Andrzej", "Nowak", 13));
+        this.patients.add(new Patient("Malgorzata", "Kolebka", 13));
+        this.patients.add(new Patient("Karlos", "Juan", 13));
+        this.patients.add(new Patient("Michal", "Michalski", 13));
     }
 
     List<Patient> getAll() {
